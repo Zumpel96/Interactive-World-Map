b4cbe719fbc42ac392f0c69caa7b321052a631ef
Zumpel96 <17179371+Zumpel96@users.noreply.github.com>
2024-02-09 20:17:08 +0100

    Added canvas panning and zooming.

---
+++ js/app.js
@@ -0,0 +1,66 @@
+const canvas = document.getElementById('map');
+const ctx = canvas.getContext('2d');
+
+let img = new Image();
+img.onload = function () {
+  draw();
+};
+img.src = '../img/world_map.jpg'; // Replace 'image-url.jpg' with the path to your image
+
+let lastX = canvas.width / 2;
+let lastY = canvas.height / 2;
+let dragging = false;
+let scale = 1;
+let offsetX = 0;
+let offsetY = 0;
+let panSpeed = 1;
+let zoomSpeed = 0.1;
+
+function draw() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+
+  ctx.translate( window.innerWidth / 2, window.innerHeight / 2 )
+  ctx.scale(scale, scale)
+  ctx.translate( -window.innerWidth / 2 + offsetX, -window.innerHeight / 2 + offsetY )
+  ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
+
+  ctx.imageSmoothingQuality = "high";
+
+  ctx.drawImage(img, 0, 0);
+}
+
+canvas.addEventListener('mousedown', function (event) {
+  lastX = event.clientX;
+  lastY = event.clientY;
+  dragging = true;
+  canvas.style.cursor = 'grabbing';
+});
+
+canvas.addEventListener('mouseup', function () {
+  dragging = false;
+  canvas.style.cursor = 'grab';
+});
+
+canvas.addEventListener('mousemove', function (event) {
+  if (dragging) {
+    let deltaX = event.clientX - lastX;
+    let deltaY = event.clientY - lastY;
+    lastX = event.clientX;
+    lastY = event.clientY;
+    offsetX += deltaX * panSpeed;
+    offsetY += deltaY * panSpeed;
+    draw();
+  }
+});
+
+canvas.addEventListener('wheel', function (event) {
+  let delta = event.deltaY > 0 ? -0.1 : 0.1;
+  scale += delta * zoomSpeed;
+  scale = Math.max(0.1, scale); // Ensure scale doesn't go below 0.1
+  draw();
+});
+
+canvas.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+});
